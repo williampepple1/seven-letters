380e999fe55b05f51030bed5d4098fe3b110f3f2
William Pepple <williampepple1@gmail.com>
2024-07-14 15:44:49 +0100

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,7 +6,7 @@ const HomePage: React.FC = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="text-center space-y-4">
-        <h1 className="text-4xl font-bold mb-6">Welcome to the Word Game</h1>
+        <h1 className="text-4xl font-bold mb-6">Welcome to the 7 Letters Word Game</h1>
         <div className="space-y-4 sm:space-y-0 sm:space-x-4">
           <Link to="/login" className="block bg-blue-500 text-white px-4 py-2 rounded sm:inline-block">
             Login with Google
