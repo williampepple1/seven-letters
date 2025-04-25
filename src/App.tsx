d468f1b13512ca74531c757dd579474fb5c5f16b
William Pepple <williampepple1@gmail.com>
2025-04-25 12:08:14 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import WordGame from './components/WordGame';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
+// import LoginPage from './pages/LoginPage';
 import GuestGame from './pages/GuestGame';
 
 const App: React.FC = () => {
@@ -10,7 +10,7 @@ const App: React.FC = () => {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
+        {/* <Route path="/login" element={<LoginPage />} /> */}
         <Route path="/game" element={<WordGame />} />
         <Route path="/guest" element={<GuestGame />} />
       </Routes>
